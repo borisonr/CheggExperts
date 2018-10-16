ce9cd8887d566b0d90f23e7e66620c591004b668
Rebecca Borison <rborison@nyml-rborison.chegg.com>
2018-10-16 15:57:11 -0400

    new build

--- dist/drive.js
+++ dist/drive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
 exports.initExpertsStore = initExpertsStore;
 exports.updateExpertsInDrive = updateExpertsInDrive;
@@ -21,12 +21,7 @@ let DRIVE;
 let fileId;
 
 function initExpertsStore() {
-  // Load client secrets from a local file.
-  fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Drive API.
-    authorize(JSON.parse(content), createExpertsStore);
-  });
+	authorize(createExpertsStore);
 }
 
 /**
@@ -35,47 +30,19 @@ function initExpertsStore() {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', code => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
+function authorize(callback) {
+	const client_secret = process.env.client_secret;
+	const client_id = process.env.client_id;
+	const redirect_uris = process.env.redirect_uris;
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+	oAuth2Client.setCredentials({
+		access_token: process.env.access_token,
+		refresh_token: process.env.refresh_token,
+		scope: "https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.file",
+		token_type: "Bearer",
+		expiry_date: process.env.expiry_date
+	});
+	callback(oAuth2Client);
 }
 
 /**
@@ -83,56 +50,56 @@ function getAccessToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 async function createExpertsStore(auth) {
-  const fileName = 'src/experts.json';
-  DRIVE = google.drive({ version: 'v3', auth });
-  const fileSize = fs.statSync(fileName).size;
-  const fileMetadata = {
-    'name': `CheggExperts${Date.now()}.docx`
-  };
+	const fileName = 'src/experts.json';
+	DRIVE = google.drive({ version: 'v3', auth });
+	const fileSize = fs.statSync(fileName).size;
+	const fileMetadata = {
+		'name': `CheggExperts${Date.now()}.docx`
+	};
 
-  const res = await DRIVE.files.create({ media: {
-      body: fs.createReadStream(fileName)
-    },
-    resource: fileMetadata
-  }, function (err, file) {
-    if (err) {
-      // Handle error
-      console.error(err);
-    } else {
-      fileId = file.data.id;
-    }
-  });
+	const res = await DRIVE.files.create({ media: {
+			body: fs.createReadStream(fileName)
+		},
+		resource: fileMetadata
+	}, function (err, file) {
+		if (err) {
+			// Handle error
+			console.error(err);
+		} else {
+			fileId = file.data.id;
+		}
+	});
 }
 
 async function updateExpertsInDrive(experts) {
-  DRIVE.files.update({
-    fileId,
-    media: {
-      body: experts
-    }
-  }, (err, file) => {
-    if (err) {
-      // Handle error
-      console.error(err);
-    } else {
-      console.log('updated experts successfully');
-    }
-  });
+	DRIVE.files.update({
+		fileId,
+		media: {
+			body: experts
+		}
+	}, (err, file) => {
+		if (err) {
+			// Handle error
+			console.error(err);
+		} else {
+			console.log('updated experts successfully');
+		}
+	});
 }
 
 async function getExpertsFromDrive() {
-  return new Promise(async (resolve, reject) => {
-    const filePath = path.join(os.tmpdir(), uuid.v4());
-    console.log(`writing to ${filePath}`);
-    const dest = fs.createWriteStream(filePath);
-    let progress = 0;
-    const res = await DRIVE.files.get({ fileId, alt: 'media' }, function (err, file) {
-      if (err) {
-        // Handle error
-        console.error(err);
-      } else {
-        resolve(file.data);
-      }
-    });
-  });
+	return new Promise(async (resolve, reject) => {
+		const filePath = path.join(os.tmpdir(), uuid.v4());
+		console.log(`writing to ${filePath}`);
+		const dest = fs.createWriteStream(filePath);
+		let progress = 0;
+		const res = await DRIVE.files.get({ fileId, alt: 'media' }, function (err, file) {
+			if (err) {
+				// Handle error
+				console.error(err);
+			} else {
+				resolve(file.data);
+			}
+		});
+	});
 }
